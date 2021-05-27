e21162b8bfffc3534182b2b048010527ad7993a1
Diego Cardoso <diegocar448@hotmail.com>
2021-05-27 14:42:22 -0300

    migration/model/route module Post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/create-post', function () {
+    $user = User::first();
+
+    $user->posts()->create([
+        'title' => Str::random(150),
+        'body' => Str::random(400),
+    ]);
+
+    return 'ok';
+});
 
 Route::get('/', function () {
     return view('welcome');
