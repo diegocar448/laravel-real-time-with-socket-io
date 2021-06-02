373bc7701bfbe8575f37fcbfae7338325bb4623f
Diego Cardoso <diegocar448@hotmail.com>
2021-06-02 15:10:03 -0300

    install vue + vue-toastify | add/config script view

--- resources/js/Echo.js
+++ resources/js/Echo.js
@@ -1,5 +1,9 @@
+import Vue from 'vue'
+
 window.Echo.channel('laravel_database_post-channel')
     .listen('PostCreated', (e) => {
         console.log(e)
         console.log(e.post)
+
+        Vue.$vToastify.success(`Titulo do post ${e.post.title}`, 'Novo Post')
     })
